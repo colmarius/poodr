dc22a28d4689d5ae703fab97d35eb3bb234ff1bc
Marius Colacioiu <marius.colacioiu@gmail.com>
2016-09-12 16:59:03 +0200

    Chapter 8 - getting started with composition

---
+++ chapter_8.rb
@@ -0,0 +1,91 @@
+class Bicycle
+  attr_reader :size, :parts
+
+  def initialize(args = {})
+    @size = args.fetch(:size)
+    @parts = args.fetch(:parts)
+  end
+
+  def spares
+    parts.spares
+  end
+end
+
+class Parts
+  attr_reader :chain, :tire_size
+
+  def initialize(args = {})
+    @chain = args.fetch(:chain, default_chain)
+    @tire_size = args.fetch(:tire_size, default_tire_size)
+    post_initialize(args)
+  end
+
+  def default_chain
+    '10-speed'
+  end
+
+  def default_tire_size
+    raise NotImplementedError
+  end
+
+  def post_initialize(args)
+  end
+
+  def spares
+    { tire_size: tire_size,
+      chain: chain }.merge(local_spares)
+  end
+
+  def local_spares
+    {}
+  end
+end
+
+class RoadBikeParts < Parts
+  attr_reader :tape_color
+
+  def post_initialize(args)
+    @tape_color = args[:tape_color]
+  end
+
+  def local_spares
+    { tape_color: tape_color }
+  end
+
+  def default_tire_size
+    '23'
+  end
+end
+
+class MountainBikeParts < Parts
+  attr_reader :front_shock, :rear_shock
+
+  def post_initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+  end
+
+  def local_spares
+    { rear_shock: rear_shock }
+  end
+
+  def default_tire_size
+    '2.1'
+  end
+end
+
+road_bike =
+  Bicycle.new(
+    size: 'L',
+    parts: RoadBikeParts.new(tape_color: 'red'))
+
+puts road_bike.size
+puts road_bike.spares
+
+mountain_bike =
+  Bicycle.new(
+    size: 'L',
+    parts: MountainBikeParts.new(rear_shock: 'Fox'))
+
+puts mountain_bike.size
+puts mountain_bike.spares
